cc8c0493c3fdaf31daf8fa07490a477fb38f6eb5
Diego Chinchilla <dchinchilla@slb.com>
2024-10-11 18:55:05 -0500

    removed configuration file

--- appConfig.ts
+++
@@ -1,6 +0,0 @@
-export const appConfig = {
-  client_id: "5f0e2a66-350f-4e9d-b2f3-aec8944567c0",
-  scope: ".default",
-  redirect_uri: "https://localhost:3000",
-  tenant_id: "5f0e2a66-350f-4e9d-b2f3-aec8944567c0",
-};
